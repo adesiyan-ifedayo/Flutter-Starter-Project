e700460f7fc5dbbfc266aa1564b8e82ebde9d9f7
Ifedayo Adesiyan <ifedayo@Ifedayos-MacBook-Pro.local>
2021-01-21 11:30:27 +0100

    dicee_app_02 - Add padding to dice images

--- dicee_app_02/lib/main.dart
+++ dicee_app_02/lib/main.dart
@@ -27,10 +27,16 @@ class DicePage extends StatelessWidget {
       child: Row(
         children: <Widget>[
           Expanded(
-            child: Image.asset('images/dice1.png'),
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Image.asset('images/dice1.png'),
+            ),
             ),
           Expanded(
-            child: Image.asset('images/dice1.png'),
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Image.asset('images/dice1.png'),
+            ),
             ),
         ]
       ),
